cba711146ab51a2bdb56303bc4edaa940f96b2d9
Bloodstone2179 <colinfrurze@gmail.com>
2024-01-04 19:49:20 +0000

    Userinput Added

--- compiler.py
+++ compiler.py
@@ -4,16 +4,17 @@ class Compiler:
     InputFile_PseudoCode_Lines = []
     includes = "#include <iostream>\n"
     C_Code = f"{includes}\nusing namespace std;\n"
-    types = ["SUBROUTINE", "OUTPUT", "END", "INVOKE", "RETURN"]
+    types = ["SUBROUTINE", "OUTPUT", "END", "INVOKE", "RETURN", "USERINPUT", "IF","FOR"]
     variable_types = ["string", "int","float"]
     vartypes_cpp = ["std::string", "int", "float"]
     functions = []
+    outType = "exe"
     
-    
-    def __init__(self, input_file: str = "program.psuedo", output_directory: str = "./output/", name: str = "Application"):
+    def __init__(self, input_file: str = "program.psuedo", output_directory: str = "./output/", name: str = "Application", outType: str = "exe"):
         self.input_file = input_file
         self.output_directory = output_directory
         self.name = name
+        self.outType = outType
     def readFile(self):
         with open(f"{os.getcwd()}/{self.input_file}", "r") as f:
             self.InputFile_PseudoCode_Lines = f.readlines()
@@ -35,14 +36,12 @@ class Compiler:
                         subName_base = lineIn_inFile.removeprefix("SUBROUTINE:").split(" ")
                         subName = subName_base[len(subName_base) - 2]
                         subArgs = lineIn_inFile.removeprefix("SUBROUTINE:" + SubRoutineType).removeprefix("(").replace(")", "").replace(subName + " THEN", "").split(",")
-                        print(f"Return Type: {SubRoutineType}  Function Name {subName} ARGS: {subArgs}")
                         parameters = ""
                         for i in subArgs:
                             i = i.split()[0]
                             parsed = i.split(":")
                             type_= parsed[-1]
                             name_ = parsed[0]
-                            print(f"Parsed: {parsed}")
                             '''
                                 type : i.s [1]
                                 name : i.s[0]
@@ -68,7 +67,6 @@ class Compiler:
                         self.C_Code += SubRoutineType + " " + subName + "() {\n"
                         func = {"name" : subName,"type": SubRoutineType, "args" : None}
                         self.functions.append(func)
-                        print(f"Return Type: {SubRoutineType}  Function Name {subName} ARGS: NULL")
                 elif self.types[1] in first_split[0] : # CHECKING IF OUTPUT
                     self.C_Code += f"std::cout << {first_split[1].removesuffix("\n")} << std::endl;\n"
                     
@@ -85,16 +83,22 @@ class Compiler:
                     varType = first_split[0].split(":")[-1]
                     varName = first_split[0].split(":")[0]
                     varContent = first_split[-1].removeprefix("<--").removesuffix("\n")
-                    print(varContent)
+                    typeIndex = self.variable_types.index(varType)
+                    cpp_equiv = self.vartypes_cpp[typeIndex]
                     if "INVOKE" in varContent.split(maxsplit=1)[0]:
                         if "(" in varContent.split(maxsplit=1)[1]:
                             varContent = f"{varContent.split(maxsplit=1)[1]}"
                         else:
                             varContent= f"{varContent.split(maxsplit=1)[1]}()"
-                    typeIndex = self.variable_types.index(varType)
-                    cpp_equiv = self.vartypes_cpp[typeIndex]
-                    print(f"TYPE: {varType}  CPP EQUIV:{cpp_equiv} NAME : {varName} DATA: {varContent}")
-                    self.C_Code += f"{cpp_equiv} {varName} = {varContent};\n"
+                        
+                        self.C_Code += f"{cpp_equiv} {varName} = {varContent};\n"
+                    elif "USERINPUT" in varContent.split(maxsplit=1)[0]:
+                        self.C_Code += f"{cpp_equiv} {varName};\nstd::getline(std::cin, {varName});\n"
+                    else:
+                        self.C_Code += f"{cpp_equiv} {varName} = {varContent};\n"
+                    
+                    
+                    
     def writeToFile(self):
         if os.path.isdir(self.output_directory) == False:
             os.mkdir(self.output_directory)
@@ -107,4 +111,4 @@ class Compiler:
         self.StartMakingC_Code()
         self.writeToFile()
         print(f"TIME TO RUN {time.time() - s} seconds")
-        subprocess.Popen(f"g++ {self.output_directory}\\{self.name}.cpp -o {self.output_directory}\\{self.name}.exe", shell=True)
+        subprocess.Popen(f"g++ {self.output_directory}\\{self.name}.cpp -o {self.output_directory}\\{self.name}.{self.outType}", shell=True)
