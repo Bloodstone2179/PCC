d7b9b4e7123150e2d823433db97c14a9f8c1dfdf
Bloodstone2179 <colinfrurze@gmail.com>
2024-03-11 18:28:00 +0000

    Updated Files
    V1.0.0.0-dev Update

--- BuildPCC.bat
+++ BuildPCC.bat
@@ -3,7 +3,5 @@ set mypath=%cd%
 set /p "version=Enter Version Name: "
 
 pyinstaller --noconfirm --onefile --console --distpath "Releases/Win64/"%version% --name "pcc" --clean --add-data "SRC/compiler.py;."  "SRC/main.py" 
-copy "./SRC/builtins.h" "./Releases/Win64/"%version%
-
-
-pause
+del VersionFile.json
+echo {"LatestReleaseVersion" : "%version%"} >> VersionFile.json
